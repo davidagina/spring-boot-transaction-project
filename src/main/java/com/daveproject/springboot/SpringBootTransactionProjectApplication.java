9a1d73bc67c7a94420203c6d0f59f980a536d0ca
davidagina <davidagina64@gmail.com>
2023-10-16 23:40:45 +0100

    Initial commit

---
+++ src/main/java/com/daveproject/springboot/SpringBootTransactionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.daveproject.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransactionProjectApplication.class, args);
+	}
+
+}
