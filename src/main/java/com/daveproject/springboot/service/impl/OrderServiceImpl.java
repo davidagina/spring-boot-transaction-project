ccc7d122b7301d3b0ac075950088d55df273d7ea
davidagina <davidagina64@gmail.com>
2023-10-17 07:22:01 +0100

    OrderService Implementation class Added

---
+++ src/main/java/com/daveproject/springboot/service/impl/OrderServiceImpl.java
@@ -0,0 +1,48 @@
+package com.daveproject.springboot.service.impl;
+
+import com.daveproject.springboot.dto.OrderRequest;
+import com.daveproject.springboot.dto.OrderResponse;
+import com.daveproject.springboot.entity.Order;
+import com.daveproject.springboot.entity.Payment;
+import com.daveproject.springboot.exception.PaymentException;
+import com.daveproject.springboot.repository.OrderRepository;
+import com.daveproject.springboot.repository.PaymentRepository;
+import com.daveproject.springboot.service.OrderService;
+
+import java.util.UUID;
+
+public class OrderServiceImpl implements OrderService {
+
+
+    private OrderRepository orderRepository;
+    private PaymentRepository paymentRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository, PaymentRepository paymentRepository){
+        this.orderRepository = orderRepository;
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    public OrderResponse  placeOrder(OrderRequest orderRequest) {
+
+        Order order = orderRequest.getOrder();
+        order.setStatus("INPROGRESS");
+        order.setOrderTrackingNumber(UUID.randomUUID().toString());
+        orderRepository.save(order);
+
+        Payment payment = orderRequest.getPayment();
+        if(!payment.getType().equals("DEBIT")){
+            throw new PaymentException("Payment card type is not supported");
+        }
+
+        payment.setOrderId(order.getId());
+        paymentRepository.save(payment);
+
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setOrderTrackingNumber(order.getOrderTrackingNumber());
+        orderResponse.setStatus(order.getStatus());
+        orderResponse.setMessage("SUCCESS");
+
+        return orderResponse;
+    }
+}
